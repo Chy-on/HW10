c0d2ef92c2a012060393aa9c6bf13045121cc487
Chy-on <213411322+Chy-on@users.noreply.github.com>
2025-09-05 11:58:59 +0900

    commit first

---
+++ Source/Test/Test.Build.cs
@@ -0,0 +1,17 @@
+using UnrealBuildTool;
+
+public class Test : ModuleRules
+{
+	public Test(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+		
+		PublicDependencyModuleNames.AddRange(new string[]
+			{
+				"Core", "CoreUObject", "Engine", "InputCore",
+			}
+		);
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
+	}
+}
